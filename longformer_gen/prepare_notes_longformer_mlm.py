06bf6aca61a2458b947d099c68caecd39bfd0ea2
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-11-05 11:34:19 -0800

    fix

--- longformer_gen/prepare_notes_longformer_mlm.py
+++ longformer_gen/prepare_notes_longformer_mlm.py
@@ -1,5 +1,5 @@
 '''
-This script pulls ../data from MIMIC-III, MIMIC-CXR.
+This script pulls data from MIMIC-III, MIMIC-CXR.
 It then 
 '''
 
@@ -10,20 +10,20 @@ from loguru import logger
 
 from sklearn.model_selection import train_test_split
 
-TRAIN_FPATH = '../data/filtered_all_notes_train.raw'
-VAL_FPATH = '../data/filtered_all_notes_val.raw'
+TRAIN_FPATH = 'data/filtered_all_notes_train.raw'
+VAL_FPATH = 'data/filtered_all_notes_val.raw'
 
 def main():
 
     admin_language = AdminLanguage()
 
     logger.info('loading MIMIC_III (Emergency Department) Note Events...')
-    notes_mimic_iii = pd.read_csv('../data/mimiciii-14/NOTEEVENTS.csv',usecols=['TEXT','ROW_ID']).rename(columns={'TEXT':'text'})
+    notes_mimic_iii = pd.read_csv('data/mimiciii-14/NOTEEVENTS.csv',usecols=['TEXT','ROW_ID']).rename(columns={'TEXT':'text'})
 
     logger.warning('subsetting for phenotype annotation task...')
 
     logger.info('Dropping rows where labels are not "sure"...')
-    mimic_iii_annot = pd.read_csv('../data/mimic-annotated/phenotype-annotations-for-patient-notes-in-the-mimic-iii-database-1.20.03/ACTdb102003.csv').drop(['SUBJECT_ID','HADM_ID','BATCH.ID','OPERATOR'],axis=1)
+    mimic_iii_annot = pd.read_csv('data/mimic-annotated/phenotype-annotations-for-patient-notes-in-the-mimic-iii-database-1.20.03/ACTdb102003.csv').drop(['SUBJECT_ID','HADM_ID','BATCH.ID','OPERATOR'],axis=1)
     mimic_iii_annot = mimic_iii_annot[mimic_iii_annot['UNSURE']==0].drop(['UNSURE'],axis=1) 
     
     notes_mimic_iii_for_annot = notes_mimic_iii[notes_mimic_iii['ROW_ID'].isin(mimic_iii_annot['ROW_ID'])]
@@ -34,13 +34,13 @@ def main():
 
     logger.info('Cleaning annotation task notes...')
     notes_mimic_iii_for_annot = preprocess_and_clean_notes(admin_language.explicit_removal,notes_mimic_iii_for_annot)
-    notes_mimic_iii_for_annot.to_csv('../data/filtered_notes_for_annotation_task.csv',index=False)
+    notes_mimic_iii_for_annot.to_csv('data/filtered_notes_for_annotation_task.csv',index=False)
 
     logger.info('Saved Annotation task Notes to One-Hot (X to y) CSV. Moving on to MLM.')
     logger.warning('Assembling data (- annotation data) for MLM pre-training...')
     logger.info('loading MIMIC_CXR (Radiology Studies) data...')
 
-    notes_mimic_cxr = pd.read_csv('../data/mimic-cxr/cxr-study-list.csv.gz',usecols=['path'])
+    notes_mimic_cxr = pd.read_csv('data/mimic-cxr/cxr-study-list.csv.gz',usecols=['path'])
 
     tqdm.pandas(desc='Assigning text for radiology studies from directory...')
     notes_mimic_cxr['text'] = notes_mimic_cxr['path'].progress_apply(get_text_from_cxr_path)
@@ -65,7 +65,7 @@ def main():
 
 
 def get_text_from_cxr_path(row):
-    fpath= '../data/mimic-cxr/'+row
+    fpath= 'data/mimic-cxr/'+row
     with open(fpath, 'r') as file:
         text = file.read()
     return text
