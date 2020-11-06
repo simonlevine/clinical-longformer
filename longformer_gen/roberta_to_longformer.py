001971c7884b6aceaf8128cd28a6ee484eb2ada6
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-11-06 15:57:11 -0800

    Update roberta_to_longformer.py

--- longformer_gen/roberta_to_longformer.py
+++ longformer_gen/roberta_to_longformer.py
@@ -132,16 +132,12 @@ def main():
     logger.info(
         f'Converting roberta-biomed-base --> {base_model_name} with global attn. window of {GLOBAL_MAX_POS} tokens.')
 
-    model, tokenizer, config = create_long_model(
-        model_specified=base_model_name_HF, attention_window=LOCAL_ATTN_WINDOW, max_pos=GLOBAL_MAX_POS)
+    model, tokenizer = create_long_model(
+        model_specified=base_model_name_HF, attention_window=LOCAL_ATTN_WINDOW, max_pos=GLOBAL_MAX_POS, save_model_to=unpretrained_model_path)
 
-    logger.info('Long model, tokenizer, and config created.')
 
-    model.save_pretrained(unpretrained_model_path) #save elongated, not pre-trained model, to the disk.
-    tokenizer.save_pretrained(unpretrained_model_path)
-    config.save_pretrained(unpretrained_model_path)
+    logger.critical(f'Long model, tokenizer created, saved to disk at {unpretrained_model_path}.')
 
-    logger.warning('SAVED elongated (but not pretrained) model, tokenizer, and config!')
 
     os.environ["CUDA_VISIBLE_DEVICES"] = "0" #GPU
 
@@ -149,10 +145,12 @@ def main():
 
     # model, tokenizer = LongformerForMaskedLM.from_pretrained('allenai/longformer-base-4096'), LongformerTokenizerFast.from_pretrained('allenai/longformer-base-4096')
 
+    logger.info(f'Loading the model from {unpretrained_model_path}')
+    tokenizer = RobertaTokenizerFast.from_pretrained(unpretrained_model_path)
+    model = RobertaLongForMaskedLM.from_pretrained(unpretrained_model_path)
     model.config.gradient_checkpointing = True #set this to ensure GPU memory constraints are OK.
 
-
-    pretrain_and_evaluate(training_args, model, tokenizer, eval_only=False, model_path=training_args.output_dir)
+    pretrain_and_evaluate(training_args, model, tokenizer, eval_only=False, model_path_out=training_args.output_dir)
 
 
     # model.save_pretrained(model_path) #save elongated AND pre-trained model, to the disk.
@@ -170,12 +168,12 @@ def main():
 
 
 
-def pretrain_and_evaluate(training_args, model, tokenizer, eval_only, model_path):
+def pretrain_and_evaluate(training_args, model, tokenizer, eval_only, model_path_out):
     logger.info(f'Loading and tokenizing data is usually slow: {VAL_FPATH}')
     val_dataset = LineByLineTextDataset(tokenizer=tokenizer,
                               file_path=VAL_FPATH,
                               block_size=tokenizer.max_len)
-
+    
     if eval_only:
         train_dataset = val_dataset
     else:
@@ -184,10 +182,6 @@ def pretrain_and_evaluate(training_args, model, tokenizer, eval_only, model_path
                                     file_path=TRAIN_FPATH,
                                     block_size= tokenizer.max_len)
 
-
-
-    model_for_mlm = RobertaLongForMaskedLM.from_pretrained(model_path)
-    tokenizer_for_mlm = RobertaTokenizerFast.from_pretrained(model_path)
 
     data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer_for_mlm, mlm=True, mlm_probability=0.15)
 
@@ -199,7 +193,7 @@ def pretrain_and_evaluate(training_args, model, tokenizer, eval_only, model_path
     logger.info(f'Initial eval bpc: {eval_loss/math.log(2)}')
     
     if not eval_only:
-        trainer.train(model_path=model_path)
+        trainer.train(model_path=model_path_out)
         trainer.save_model()
 
         eval_loss = trainer.evaluate()
@@ -244,7 +238,7 @@ def copy_proj_layers(model):
 
 
 
-def create_long_model(model_specified, attention_window, max_pos):
+def create_long_model(model_specified, attention_window, max_pos, save_model_to):
 
     """Starting from the `roberta-base` (or similar) checkpoint, the following function converts it into an instance of `RobertaLong`.
      It makes the following changes:
@@ -306,7 +300,11 @@ def create_long_model(model_specified, attention_window, max_pos):
 
         layer.attention.self = longformer_self_attn
 
-    return model, tokenizer, config
+    logger.info(f'saving model to {save_model_to}')
+    model.save_pretrained(save_model_to)
+    tokenizer.save_pretrained(save_model_to)
+
+    return model, tokenizer
 
 
 class RobertaLongSelfAttention(LongformerSelfAttention):
