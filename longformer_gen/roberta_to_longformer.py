0b4bb3a6625b6859da9018b0a248fc141bcb78f3
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-11-06 15:43:48 -0800

    Update roberta_to_longformer.py

--- longformer_gen/roberta_to_longformer.py
+++ longformer_gen/roberta_to_longformer.py
@@ -169,150 +169,6 @@ def main():
 
 
 
-class CustomSelfAttention(LongformerSelfAttention):
-    def forward(
-        self, hidden_states, attention_mask=None, is_index_masked=None, is_index_global_attn=None, is_global_attn=None,*args
-    ):
-        """
-        LongformerSelfAttention expects `len(hidden_states)` to be multiple of `attention_window`. Padding to
-        `attention_window` happens in LongformerModel.forward to avoid redoing the padding on each layer.
-        The `attention_mask` is changed in `BertModel.forward` from 0, 1, 2 to -ve: no attention
-              0: local attention
-            +ve: global attention
-        """
-        hidden_states = hidden_states.transpose(0, 1)
-
-        # project hidden states
-        query_vectors = self.query(hidden_states)
-        key_vectors = self.key(hidden_states)
-        value_vectors = self.value(hidden_states)
-
-        seq_len, batch_size, embed_dim = hidden_states.size()
-        assert (
-            embed_dim == self.embed_dim
-        ), f"hidden_states should have embed_dim = {self.embed_dim}, but has {embed_dim}"
-
-        # normalize query
-        query_vectors /= math.sqrt(self.head_dim)
-
-        query_vectors = query_vectors.view(seq_len, batch_size, self.num_heads, self.head_dim).transpose(0, 1)
-        key_vectors = key_vectors.view(seq_len, batch_size, self.num_heads, self.head_dim).transpose(0, 1)
-
-        attn_scores = self._sliding_chunks_query_key_matmul(
-            query_vectors, key_vectors, self.one_sided_attn_window_size
-        )
-
-        # values to pad for attention probs
-        remove_from_windowed_attention_mask = (attention_mask != 0)[:, :, None, None]
-
-        # cast to fp32/fp16 then replace 1's with -inf
-        float_mask = remove_from_windowed_attention_mask.type_as(query_vectors).masked_fill(
-            remove_from_windowed_attention_mask, -10000.0
-        )
-        # diagonal mask with zeros everywhere and -inf inplace of padding
-        diagonal_mask = self._sliding_chunks_query_key_matmul(
-            float_mask.new_ones(size=float_mask.size()), float_mask, self.one_sided_attn_window_size
-        )
-
-        # pad local attention probs
-        attn_scores += diagonal_mask
-
-        assert list(attn_scores.size()) == [
-            batch_size,
-            seq_len,
-            self.num_heads,
-            self.one_sided_attn_window_size * 2 + 1,
-        ], f"local_attn_probs should be of size ({batch_size}, {seq_len}, {self.num_heads}, {self.one_sided_attn_window_size * 2 + 1}), but is of size {attn_scores.size()}"
-
-        # compute local attention probs from global attention keys and contact over window dim
-        if is_global_attn:
-            # compute global attn indices required through out forward fn
-            (
-                max_num_global_attn_indices,
-                is_index_global_attn_nonzero,
-                is_local_index_global_attn_nonzero,
-                is_local_index_no_global_attn_nonzero,
-            ) = self._get_global_attn_indices(is_index_global_attn)
-            # calculate global attn probs from global key
-
-            global_key_attn_scores = self._concat_with_global_key_attn_probs(
-                query_vectors=query_vectors,
-                key_vectors=key_vectors,
-                max_num_global_attn_indices=max_num_global_attn_indices,
-                is_index_global_attn_nonzero=is_index_global_attn_nonzero,
-                is_local_index_global_attn_nonzero=is_local_index_global_attn_nonzero,
-                is_local_index_no_global_attn_nonzero=is_local_index_no_global_attn_nonzero,
-            )
-            # concat to local_attn_probs
-            # (batch_size, seq_len, num_heads, extra attention count + 2*window+1)
-            attn_scores = torch.cat((global_key_attn_scores, attn_scores), dim=-1)
-
-            # free memory
-            del global_key_attn_scores
-
-        local_attn_probs_fp32 = F.softmax(attn_scores, dim=-1, dtype=torch.float32)  # use fp32 for numerical stability
-        local_attn_probs = local_attn_probs_fp32.type_as(attn_scores)
-
-        # free memory
-        del local_attn_probs_fp32
-
-        # softmax sometimes inserts NaN if all positions are masked, replace them with 0
-        local_attn_probs = torch.masked_fill(local_attn_probs, is_index_masked[:, :, None, None], 0.0)
-
-        # apply dropout
-        local_attn_probs = F.dropout(local_attn_probs, p=self.dropout, training=self.training)
-
-        value_vectors = value_vectors.view(seq_len, batch_size, self.num_heads, self.head_dim).transpose(0, 1)
-
-        # compute local attention output with global attention value and add
-        if is_global_attn:
-            # compute sum of global and local attn
-            attn_output = self._compute_attn_output_with_global_indices(
-                value_vectors=value_vectors,
-                attn_probs=local_attn_probs,
-                max_num_global_attn_indices=max_num_global_attn_indices,
-                is_index_global_attn_nonzero=is_index_global_attn_nonzero,
-                is_local_index_global_attn_nonzero=is_local_index_global_attn_nonzero,
-            )
-        else:
-            # compute local attn only
-            attn_output = self._sliding_chunks_matmul_attn_probs_value(
-                local_attn_probs, value_vectors, self.one_sided_attn_window_size
-            )
-
-        assert attn_output.size() == (batch_size, seq_len, self.num_heads, self.head_dim), "Unexpected size"
-        attn_output = attn_output.transpose(0, 1).reshape(seq_len, batch_size, embed_dim).contiguous()
-
-        # compute value for global attention and overwrite to attention output
-        # TODO: remove the redundant computation
-        if is_global_attn:
-            global_attn_output, global_attn_probs = self._compute_global_attn_output_from_hidden(
-                hidden_states=hidden_states,
-                max_num_global_attn_indices=max_num_global_attn_indices,
-                is_local_index_global_attn_nonzero=is_local_index_global_attn_nonzero,
-                is_index_global_attn_nonzero=is_index_global_attn_nonzero,
-                is_local_index_no_global_attn_nonzero=is_local_index_no_global_attn_nonzero,
-                is_index_masked=is_index_masked,
-            )
-
-            # get only non zero global attn output
-            nonzero_global_attn_output = global_attn_output[
-                is_local_index_global_attn_nonzero[0], :, is_local_index_global_attn_nonzero[1]
-            ]
-
-            # overwrite values with global attention
-            attn_output[is_index_global_attn_nonzero[::-1]] = nonzero_global_attn_output.view(
-                len(is_local_index_global_attn_nonzero[0]), -1
-            )
-            # The attention weights for tokens with global attention are
-            # just filler values, they were never used to compute the output.
-            # Fill with 0 now, the correct values are in 'global_attn_probs'.
-            local_attn_probs[is_index_global_attn_nonzero] = 0
-
-        outputs = (attn_output.transpose(0, 1), local_attn_probs)
-
-        return outputs + (global_attn_probs,) if is_global_attn else outputs
-    
 
 def pretrain_and_evaluate(training_args, model, tokenizer, eval_only, model_path):
     logger.info(f'Loading and tokenizing data is usually slow: {VAL_FPATH}')
@@ -328,9 +184,11 @@ def pretrain_and_evaluate(training_args, model, tokenizer, eval_only, model_path
                                     file_path=TRAIN_FPATH,
                                     block_size= tokenizer.max_len)
 
+
     data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=True, mlm_probability=0.15)
-    trainer = Trainer(model=model, args=training_args, data_collator=data_collator,
-                      train_dataset=train_dataset, eval_dataset=val_dataset, prediction_loss_only=True,)
+
+    trainer = Trainer(model=model, args=None, data_collator=data_collator,
+                      train_dataset=train_dataset, eval_dataset=val_dataset, prediction_loss_only=True)
 
     eval_loss = trainer.evaluate()
     eval_loss = eval_loss['eval_loss']
@@ -433,7 +291,7 @@ def create_long_model(model_specified, attention_window, max_pos):
     # replace the `modeling_bert.BertSelfAttention` object with `LongformerSelfAttention`
     config.attention_window = [attention_window] * config.num_hidden_layers
     for i, layer in enumerate(model.roberta.encoder.layer):
-        longformer_self_attn = CustomSelfAttention(config, layer_id=i)
+        longformer_self_attn = RobertaLongSelfAttention(config, layer_id=i)
         longformer_self_attn.query = copy.deepcopy(layer.attention.self.query)
         longformer_self_attn.key = copy.deepcopy(layer.attention.self.key)
         longformer_self_attn.value = copy.deepcopy(layer.attention.self.value)
@@ -447,30 +305,30 @@ def create_long_model(model_specified, attention_window, max_pos):
     return model, tokenizer, config
 
 
-# class RobertaLongSelfAttention(LongformerSelfAttention):
-#     '''
-#     Inherits above...
-#     '''
-#     def forward(
-#         self,
-#         hidden_states,
-#         attention_mask=None,
-#         head_mask=None,
-#         encoder_hidden_states=None,
-#         encoder_attention_mask=None,
-#         output_attentions=False,
-#         ):
-#         return super().forward(hidden_states, attention_mask=attention_mask, output_attentions=output_attentions)
+class RobertaLongSelfAttention(LongformerSelfAttention):
+    '''
+    Inherits above...
+    '''
+    def forward(
+        self,
+        hidden_states,
+        attention_mask=None,
+        head_mask=None,
+        encoder_hidden_states=None,
+        encoder_attention_mask=None,
+        output_attentions=False,
+        ):
+        return super().forward(hidden_states, attention_mask=attention_mask, output_attentions=output_attentions)
 
-# class RobertaLongForMaskedLM(RobertaForMaskedLM):
-#     """RobertaLongForMaskedLM represents the "long" version of the RoBERTa model.
-#      It replaces BertSelfAttention with RobertaLongSelfAttention, which is 
-#      a thin wrapper around LongformerSelfAttention."""
-#     def __init__(self, config):
-#         super().__init__(config)
-#         for i, layer in enumerate(self.encoder.layer):
-#             # replace the `modeling_bert.BertSelfAttention` object with `RobertaLongSelfAttention`
-#             layer.attention.self = RobertaLongSelfAttention(config, layer_id=i)
+class RobertaLongForMaskedLM(RobertaForMaskedLM):
+    """RobertaLongForMaskedLM represents the "long" version of the RoBERTa model.
+     It replaces BertSelfAttention with RobertaLongSelfAttention, which is 
+     a thin wrapper around LongformerSelfAttention."""
+    def __init__(self, config):
+        super().__init__(config)
+        for i, layer in enumerate(self.encoder.layer):
+            # replace the `modeling_bert.BertSelfAttention` object with `RobertaLongSelfAttention`
+            layer.attention.self = RobertaLongSelfAttention(config, layer_id=i)
 
 
 
