5f6ac385ec6b039f9d9f49216e811067e8d2ebed
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-11-07 19:51:13 -0800

    add

--- longformer_gen/pretrain_roberta_long.py
+++ longformer_gen/pretrain_roberta_long.py
@@ -4,7 +4,7 @@ import os
 import copy
 import math
 from dataclasses import dataclass, field
-from transformers import RobertaForMaskedLM, RobertaTokenizerFast, DataCollatorForLanguageModeling, Trainer, TextDataset
+from transformers import RobertaForMaskedLM, RobertaTokenizerFast, DataCollatorForLanguageModeling, Trainer
 from transformers import TrainingArguments, HfArgumentParser
 
 from torch.utils.data import Dataset
@@ -16,7 +16,22 @@ import warnings
 
 import torch
 import torch.nn as nn
+from torch.nn import CrossEntropyLoss, MSELoss
+from torch.nn import functional as F
 
+
+import json
+import os
+import pickle
+import random
+import time
+import warnings
+from typing import Dict, List, Optional
+
+import torch
+from torch.utils.data.dataset import Dataset
+
+from filelock import FileLock
 import pandas as pd
 
 
@@ -25,8 +40,9 @@ TRAIN_FPATH = 'data/filtered_all_notes_train.txt'
 VAL_FPATH = 'data/filtered_all_notes_val.txt'
 SAMPLE_FPATH = 'data/filtered_all_notes_SAMPLE.txt'
 
-MODEL_OUT_DIR = './roberta_gen'
+MODEL_OUT_DIR = './longformer_gen'
 LOCAL_ATTN_WINDOW = 512 #params['local_attention_window']
+GLOBAL_MAX_POS = 4096 #params['global_attention_window']
 
 FAST_DEV_RUN = False
 
@@ -38,40 +54,42 @@ if FAST_DEV_RUN == True:
     VAL_FPATH = SAMPLE_FPATH
 
 
+
 def main():
 
     if FAST_DEV_RUN == True:
 
         training_args = TrainingArguments(
-            output_dir="./roberta_gen/checkpoints",
+            output_dir="./longformer_gen/checkpoints",
             overwrite_output_dir=True,
             max_steps=1,
-            warmup_steps= 0,
+            warmup_steps= 0, #-->3000
             logging_steps=1,
             save_steps=1,
             max_grad_norm= 5.0,
-            per_device_eval_batch_size=128,
-            per_device_train_batch_size=64,
+            per_device_eval_batch_size=2,
+            per_device_train_batch_size=1,
             gradient_accumulation_steps= 32,
             learning_rate = 0.00003,
             adam_epsilon= 1e-6,
             weight_decay= 0.01,
             do_eval= True,
             do_train=True,
+            fp16=True
             )
     
     elif FAST_DEV_RUN == False:
 
         training_args = TrainingArguments(
-        output_dir="./roberta_gen/checkpoints",
+        output_dir=f"./longformer_gen/checkpoints/bioclinicaLongformer",
         overwrite_output_dir=True,
         warmup_steps= 500,
         logging_steps=500,
-        max_steps = 5000,
+        max_steps = 3000,
         save_steps=500,
         max_grad_norm= 5.0,
-        per_device_eval_batch_size=64,
-        per_device_train_batch_size=32,
+        per_device_eval_batch_size=2,
+        per_device_train_batch_size=1,
         gradient_accumulation_steps= 32,
         learning_rate = 0.00003,
         adam_epsilon= 1e-6,
@@ -84,21 +102,26 @@ def main():
     base_model_name_HF = 'allenai/biomed_roberta_base' #params['base_model_name']
 
     base_model_name = base_model_name_HF.split('/')[-1]
-    model_path = f'{MODEL_OUT_DIR}/bioclinical-roberta'
-    unpretrained_model_path = f'{MODEL_OUT_DIR}/{base_model_name}' #includes speedfix
+    model_path = f'{MODEL_OUT_DIR}/bioclinical-longformer' #includes speedfix
+    unpretrained_model_path = f'{MODEL_OUT_DIR}/{base_model_name}-{GLOBAL_MAX_POS}' #includes speedfix
 
     logger.info(f'Loading the model from {unpretrained_model_path}')
-    tokenizer = RobertaTokenizerFast.from_pretrained(unpretrained_model_path)
-    model = RobertaLongForMaskedLM.from_pretrained(unpretrained_model_path) #,gradient_checkpointing=True)
+    tokenizer = RobertaTokenizerFast.from_pretrained(unpretrained_model_path,model_max_length=GLOBAL_MAX_POS)
+    model = RobertaLongForMaskedLM.from_pretrained(unpretrained_model_path,gradient_checkpointing=True)
 
     logger.warning(f'Tokenizer {tokenizer} parameterized with model_max_len as {tokenizer.model_max_length}')
+
+    model.config.gradient_checkpointing = True #set this to ensure GPU memory constraints are OK.
     
-    logger.critical(f'Pre-Training {model.num_parameters()}-parameter model. This could take days!!!!')
+    logger.critical(f'Pre-Training {model.num_parameters()}-parameter model. This could take ~ 2-3 days!!!!')
     pretrain_and_evaluate(training_args, model, tokenizer, eval_only=False, model_path_out=training_args.output_dir)
 
+    logger.warning(f'Copying local projection layers into global projection layers ... ')
+    model = copy_proj_layers(model)
     logger.warning(f'Saving model to {model_path}/final')
     model.save_pretrained(f'{model_path}/final') # --> "./longformer_gen/bioclinicaLongformer/final"
     logger.critical('Final pre-trained model, tokenizer,and config saved!')
+
 
 
 class RobertaLongSelfAttention(LongformerSelfAttention):
@@ -122,20 +145,53 @@ class RobertaLongForMaskedLM(RobertaForMaskedLM):
             layer.attention.self = RobertaLongSelfAttention(config, layer_id=i)
 
 
+class LineByLineTextDataset(Dataset):
+    """
+    This will be superseded by a framework-agnostic approach soon.
+    """
+
+    def __init__(self, tokenizer, file_path: str, block_size: int):
+
+
+        logger.warning(f'Block size in dataset set as {block_size}')
+        # warnings.warn(DEPRECATION_WARNING, FutureWarning)
+        assert os.path.isfile(file_path), f"Input file path {file_path} not found"
+        # Here, we do not cache the features, operating under the assumption
+        # that we will soon use fast multithreaded tokenizers from the
+        # `tokenizers` repo everywhere =)
+        logger.info(f'Creating features from dataset file at {file_path}')
+
+        with open(file_path, encoding="utf-8") as f:
+            lines = [line for line in f.read().splitlines() if (len(line) > 10 and not line.isspace())]
+
+        batch_encoding = tokenizer(lines, add_special_tokens=True, truncation=True, max_length=block_size, padding='max_length')
+        
+        self.examples = batch_encoding["input_ids"]
+        self.examples = [{"input_ids": torch.tensor(e, dtype=torch.long)} for e in self.examples]
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, i) -> Dict[str, torch.tensor]:
+        return self.examples[i]
+
+
+
 def pretrain_and_evaluate(training_args, model, tokenizer, eval_only, model_path_out):
     logger.info(f'Loading and tokenizing data is usually slow: {VAL_FPATH}')
 
-    val_dataset = TextDataset(tokenizer=tokenizer,
+    val_dataset = LineByLineTextDataset(tokenizer=tokenizer,
                               file_path=VAL_FPATH,
-                              block_size=tokenizer.max_len)
+                              block_size=GLOBAL_MAX_POS)
+
 
     if eval_only:
         train_dataset = val_dataset
     else:
         logger.info(f'Loading and tokenizing training data is usually slow: {TRAIN_FPATH}')
-        train_dataset = TextDataset(tokenizer=tokenizer,
+        train_dataset = LineByLineTextDataset(tokenizer=tokenizer,
                                     file_path=TRAIN_FPATH,
-                                    block_size= tokenizer.max_len)
+                                    block_size= GLOBAL_MAX_POS)
 
 
     data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=True, mlm_probability=0.15)
@@ -156,6 +212,9 @@ def pretrain_and_evaluate(training_args, model, tokenizer, eval_only, model_path
         eval_loss = trainer.evaluate()
         eval_loss = eval_loss['eval_loss']
         logger.info(f'Eval bpc after pretraining: {eval_loss/math.log(2)}')
+
+
+
 
 def copy_proj_layers(model):
     for i, layer in enumerate(model.roberta.encoder.layer):
