1b177a6dd6df10289049512108a5f45adfdb9df3
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-11-07 21:07:32 -0800

    Update pretrain_roberta.py

--- longformer_gen/pretrain_roberta.py
+++ longformer_gen/pretrain_roberta.py
@@ -4,7 +4,7 @@ import os
 import copy
 import math
 from dataclasses import dataclass, field
-from transformers import RobertaForMaskedLM, RobertaTokenizerFast, DataCollatorForLanguageModeling, Trainer, TextDataset
+from transformers import RobertaForMaskedLM, RobertaTokenizer, DataCollatorForLanguageModeling, Trainer, TextDataset
 from transformers import TrainingArguments, HfArgumentParser
 
 from torch.utils.data import Dataset
@@ -89,7 +89,7 @@ def main():
 
     logger.info(f'Loading the model from {unpretrained_model_path}')
 
-    tokenizer = RobertaTokenizerFast.from_pretrained(unpretrained_model_path)
+    tokenizer = RobertaTokenizer.from_pretrained(unpretrained_model_path)
     model = RobertaForMaskedLM.from_pretrained(unpretrained_model_path, gradient_checkpointing=True)
 
     logger.warning(f'Tokenizer {tokenizer} parameterized with model_max_len as {tokenizer.model_max_length}')
