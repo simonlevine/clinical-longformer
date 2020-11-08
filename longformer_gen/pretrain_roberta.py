f3a545e9619f1f8cbaf7574dbcf0ef483ade14d5
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-11-07 20:36:56 -0800

    Update pretrain_roberta.py

--- longformer_gen/pretrain_roberta.py
+++ longformer_gen/pretrain_roberta.py
@@ -49,8 +49,8 @@ def main():
             logging_steps=1,
             save_steps=1,
             max_grad_norm= 5.0,
-            per_device_eval_batch_size=4, #4,4 WORKS
-            per_device_train_batch_size=8,
+            per_device_eval_batch_size=32, #4,4 WORKS
+            per_device_train_batch_size=32,
             gradient_accumulation_steps= 32,
             learning_rate = 0.00003,
             adam_epsilon= 1e-6,
