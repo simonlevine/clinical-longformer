bcaeb6a262a3a7819b9b6bf4da7495a656217c6c
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-11-08 12:56:17 -0800

    add

--- classifier_pipeline/classifier_one_label.py
+++ classifier_pipeline/classifier_one_label.py
@@ -57,6 +57,7 @@ class Classifier(pl.LightningModule):
             df = df[["TEXT", "ICD9_CODE"]]
             df = df.rename(columns={'TEXT':'text', 'ICD9_CODE':'label'})
             top_fifty_codes=df['label'].value_counts()[:50].index.tolist()
+            logger.warning(f'Predicting the top 50 most frequent ICD codes: {top_fifty_codes}')
 
             df = df[df['label'].isin(top_fifty_codes)]
             
