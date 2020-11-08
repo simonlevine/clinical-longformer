960eece8d3a3f087dedb765df59228519de80d90
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-11-08 12:31:18 -0800

    Update download_and_unzip_mimicdata.sh

--- download_and_unzip_mimicdata.sh
+++ download_and_unzip_mimicdata.sh
@@ -9,7 +9,7 @@ cd data
 
 wget -r -N -c -np --user slevineg --password Mivdo9-merjuh-gokwuc https://physionet.org/files/mimiciii/1.4/NOTEEVENTS.csv.gz
 wget -r -N -c -np --user slevineg --password Mivdo9-merjuh-gokwuc https://physionet.org/files/mimiciii/1.4/PROCEDURES_ICD.csv.gz
-wget -r -N -c -np --user slevineg --password Mivdo9-merjuh-gokwuc https://physionet.org/files/mimiciii/1.4/DIAGNOSES_ICS.csv.gz
+wget -r -N -c -np --user slevineg --password Mivdo9-merjuh-gokwuc https://physionet.org/files/mimiciii/1.4/DIAGNOSES_ICD.csv.gz
 
 wget -r -N -c -np --user slevineg --password Mivdo9-merjuh-gokwuc https://physionet.org/files/mimiciii/1.4/D_ICD_DIAGNOSES.csv.gz
 wget -r -N -c -np --user slevineg --password Mivdo9-merjuh-gokwuc https://physionet.org/files/mimiciii/1.4/D_ICD_PROCEDURES.csv.gz
