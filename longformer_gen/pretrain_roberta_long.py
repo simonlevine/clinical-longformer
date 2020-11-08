5a5b7cb0977ac28b4a18095760d028bd5fec8ab3
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-11-07 20:07:27 -0800

    Update pretrain_roberta_long.py

--- longformer_gen/pretrain_roberta_long.py
+++ longformer_gen/pretrain_roberta_long.py
@@ -44,7 +44,7 @@ MODEL_OUT_DIR = './longformer_gen'
 LOCAL_ATTN_WINDOW = 512 #params['local_attention_window']
 GLOBAL_MAX_POS = 4096 #params['global_attention_window']
 
-FAST_DEV_RUN = False
+FAST_DEV_RUN = True
 
 if FAST_DEV_RUN == True:
 
