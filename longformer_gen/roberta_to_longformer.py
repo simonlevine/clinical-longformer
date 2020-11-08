e6ad5eb385835bc3a4665dddb1980095bec6895f
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-11-07 18:57:10 -0800

    split script

--- longformer_gen/roberta_to_longformer.py
+++ longformer_gen/roberta_to_longformer.py
@@ -134,31 +134,6 @@ def main():
 
     logger.critical(f'Long model, tokenizer created, saved to disk at {unpretrained_model_path}.')
 
-
-    os.environ["CUDA_VISIBLE_DEVICES"] = "0" #GPU
-
-    logger.info(f'Pretraining roberta-biomed-{GLOBAL_MAX_POS} ... ')
-
-    # model, tokenizer = LongformerForMaskedLM.from_pretrained('allenai/longformer-base-4096'), LongformerTokenizerFast.from_pretrained('allenai/longformer-base-4096')
-
-    logger.info(f'Loading the model from {unpretrained_model_path}')
-    tokenizer = RobertaTokenizerFast.from_pretrained(unpretrained_model_path,model_max_length=GLOBAL_MAX_POS)
-    model = RobertaLongForMaskedLM.from_pretrained(unpretrained_model_path,gradient_checkpointing=True)
-
-    logger.warning(f'Tokenizer {tokenizer} parameterized with model_max_len as {tokenizer.model_max_length}')
-
-    model.config.gradient_checkpointing = True #set this to ensure GPU memory constraints are OK.
-    
-    logger.critical(f'Pre-Training {model.num_parameters()}-parameter model. This could take ~ 2-3 days!!!!')
-    pretrain_and_evaluate(training_args, model, tokenizer, eval_only=False, model_path_out=training_args.output_dir)
-
-    logger.warning(f'Copying local projection layers into global projection layers ... ')
-    model = copy_proj_layers(model)
-    logger.warning(f'Saving model to {model_path}/final')
-    model.save_pretrained(f'{model_path}/final') # --> "./longformer_gen/bioclinicaLongformer/final"
-    logger.critical('Final pre-trained model, tokenizer,and config saved!')
-
-
 def pretrain_and_evaluate(training_args, model, tokenizer, eval_only, model_path_out):
     logger.info(f'Loading and tokenizing data is usually slow: {VAL_FPATH}')
 
@@ -194,46 +169,6 @@ def pretrain_and_evaluate(training_args, model, tokenizer, eval_only, model_path
         eval_loss = trainer.evaluate()
         eval_loss = eval_loss['eval_loss']
         logger.info(f'Eval bpc after pretraining: {eval_loss/math.log(2)}')
-
-
-
-class LineByLineTextDataset(Dataset):
-    """
-    This will be superseded by a framework-agnostic approach soon.
-    """
-
-    def __init__(self, tokenizer, file_path: str, block_size: int):
-
-
-        logger.warning(f'Block size in dataset set as {block_size}')
-        # warnings.warn(DEPRECATION_WARNING, FutureWarning)
-        assert os.path.isfile(file_path), f"Input file path {file_path} not found"
-        # Here, we do not cache the features, operating under the assumption
-        # that we will soon use fast multithreaded tokenizers from the
-        # `tokenizers` repo everywhere =)
-        logger.info(f'Creating features from dataset file at {file_path}')
-
-        with open(file_path, encoding="utf-8") as f:
-            lines = [line for line in f.read().splitlines() if (len(line) > 10 and not line.isspace())]
-
-        batch_encoding = tokenizer(lines, add_special_tokens=True, truncation=True, max_length=block_size, padding='max_length')
-        
-        self.examples = batch_encoding["input_ids"]
-        self.examples = [{"input_ids": torch.tensor(e, dtype=torch.long)} for e in self.examples]
-
-    def __len__(self):
-        return len(self.examples)
-
-    def __getitem__(self, i) -> Dict[str, torch.tensor]:
-        return self.examples[i]
-
-
-def copy_proj_layers(model):
-    for i, layer in enumerate(model.roberta.encoder.layer):
-        layer.attention.self.query_global = layer.attention.self.query
-        layer.attention.self.key_global = layer.attention.self.key
-        layer.attention.self.value_global = layer.attention.self.value
-    return model
 
 
 
@@ -306,27 +241,6 @@ def create_long_model(model_specified, attention_window, max_pos, save_model_to)
     tokenizer.save_pretrained(save_model_to)
 
     return model, tokenizer
-
-
-class RobertaLongSelfAttention(LongformerSelfAttention):
-    def forward(
-        self,
-        hidden_states,
-        attention_mask=None,
-        head_mask=None,
-        encoder_hidden_states=None,
-        encoder_attention_mask=None,
-        output_attentions=False,
-    ):
-        return super().forward(hidden_states, attention_mask=attention_mask, output_attentions=output_attentions)
-
-
-class RobertaLongForMaskedLM(RobertaForMaskedLM):
-    def __init__(self, config):
-        super().__init__(config)
-        for i, layer in enumerate(self.roberta.encoder.layer):
-            # replace the `modeling_bert.BertSelfAttention` object with `LongformerSelfAttention`
-            layer.attention.self = RobertaLongSelfAttention(config, layer_id=i)
 
 if __name__ == "__main__":
     main()
