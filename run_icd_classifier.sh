2ca38997e3d6d6482c0fd1b576efaf12977160eb
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-11-11 15:29:31 -0800

    Update run_icd_classifier.sh

--- run_icd_classifier.sh
+++ run_icd_classifier.sh
@@ -4,4 +4,5 @@ python classifier_pipeline/training_onelabel.py \
     --transformer_type roberta-long \
     --encoder_model simonlevine/bioclinical-roberta-long \
     --batch_size 2 \
-    --gpus 1
+    --gpus 1 \
+    --fast_dev_run True
